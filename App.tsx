f02d8caeb5d56daaee796dccb99d4b511e5f5009
Mike Giffin <giffinmike@Mikes-MacBook-Pro.local>
2025-05-24 20:45:56 -0700

    change1

--- App.tsx
+++ App.tsx
@@ -68,7 +68,7 @@ const App = () => {
     <SafeAreaView style={backgroundStyle}>
         <Section title="Step One">
             Eddsfgdfgit <Text style={styles.highlight}>App.tsx</Text> to change this
-            asdfsadfsdf
+     sssssssssss
            </Section>
     </SafeAreaView>
   );
