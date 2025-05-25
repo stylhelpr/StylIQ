46bfe20b26a2b67c14f5738c769f18a8f455d31d
Mike Giffin <giffinmike@Mikes-MacBook-Pro.local>
2025-05-24 20:27:18 -0700

    Initial commit for StylIQ React Native app

---
+++ metro.config.js
@@ -0,0 +1,36 @@
+// metro.config.js
+const { getDefaultConfig } = require('metro-config');
+const path = require('path');
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+
+  return {
+    projectRoot: __dirname,
+    watchFolders: [path.resolve(__dirname)],
+    resolver: {
+      assetExts,
+      sourceExts,
+    },
+    maxWorkers: 1,
+  };
+})();
+
+
+
+
+
+// // metro.config.js
+// const { getDefaultConfig } = require('metro-config');
+
+// module.exports = (async () => {
+//   const defaultConfig = await getDefaultConfig();
+
+//   return {
+//     ...defaultConfig,
+//     maxWorkers: 1, // Prevent overwhelming file handles
+//     watchFolders: [__dirname],
+//   };
+// })();
