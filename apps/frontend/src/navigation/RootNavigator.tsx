7c8db7940b3ba792cde8e9356f2b9c9f607ba72c
Mike Giffin <giffinmike@Mikes-MacBook-Pro.local>
2025-05-26 23:08:23 -0700

    all working again with simpler nav2

--- apps/frontend/src/navigation/RootNavigator.tsx
+++ apps/frontend/src/navigation/RootNavigator.tsx
@@ -1,3 +1,42 @@
+// // RootNavigator.tsx
+// import React, {useState} from 'react';
+// import HomeScreen from '../screens/HomeScreen';
+// import ProfileScreen from '../screens/ProfileScreen';
+
+// export type Screen = 'Home' | 'Profile';
+
+// interface ScreenResult {
+//   screen: Screen;
+//   component: JSX.Element;
+// }
+
+// const useCustomRouter = (): [
+//   JSX.Element,
+//   (screen: Screen, params?: {userId: string}) => void,
+// ] => {
+//   const [currentScreen, setCurrentScreen] = useState<Screen>('Home');
+//   const [userId, setUserId] = useState<string | null>(null);
+
+//   const navigate = (screen: Screen, params?: {userId: string}) => {
+//     if (screen === 'Profile' && params?.userId) setUserId(params.userId);
+//     setCurrentScreen(screen);
+//   };
+
+//   let content: JSX.Element;
+
+//   if (currentScreen === 'Profile' && userId) {
+//     content = <ProfileScreen userId={userId} navigate={navigate} />;
+//   } else {
+//     content = <HomeScreen navigate={navigate} />;
+//   }
+
+//   return [content, navigate];
+// };
+
+// export default useCustomRouter;
+
+//////////
+
 // RootNavigator.tsx
 import React, {useState} from 'react';
 import HomeScreen from '../screens/HomeScreen';
