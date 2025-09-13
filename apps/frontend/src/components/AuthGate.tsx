549849ae71d010f1ccada3d1ce6d1913bc46420a
Your Name <you@example.com>
2025-09-13 12:11:02 -0700

    working1

--- apps/frontend/src/components/AuthGate.tsx
+++ apps/frontend/src/components/AuthGate.tsx
@@ -24,32 +24,3 @@ export default function AuthGate({children}: Props) {
 
   return <>{children}</>;
 }
-
-//////////////////
-
-// import React from 'react';
-// import {View, ActivityIndicator} from 'react-native';
-// import {useAuth0} from 'react-native-auth0';
-// import LoginScreen from 'screens/LoginScreen';
-
-// type Props = {
-//   children: React.ReactNode;
-// };
-
-// export default function AuthGate({children}: Props) {
-//   const {user, isLoading} = useAuth0();
-
-//   if (isLoading) {
-//     return (
-//       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-//         <ActivityIndicator size="large" />
-//       </View>
-//     );
-//   }
-
-//   if (!user) {
-//     return <LoginScreen />;
-//   }
-
-//   return <>{children}</>;
-// }
