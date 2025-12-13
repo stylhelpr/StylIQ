d143d10b63471445f4d18e9bc030c39dc0e8e10d
giffinmike <mike@stylhelpr.com>
2025-12-12 18:41:54 -0800

    all animations1

--- apps/frontend/src/screens/AiStyleChatScreen.tsx
+++ apps/frontend/src/screens/AiStyleChatScreen.tsx
@@ -1,4 +1,4 @@
-// /* eslint-disable react-native/no-inline-styles */
+// /* eslint-disable react-native/no-inline-styles *//
 import React, {useMemo, useRef, useState, useEffect, useCallback} from 'react';
 import {
   View,
