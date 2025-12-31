e4f39339864098c02ade17e6ba2197ee4c287634
giffinmike <mike@stylhelpr.com>
2025-12-30 19:54:25 -0800

    before shopping controller1

--- apps/backend-nest/src/gcs/gcs.controller.ts
+++ apps/backend-nest/src/gcs/gcs.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { GCSService } from './gcs.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('upload-url')
 export class GCSController {
   constructor(private readonly gcsService: GCSService) {}
